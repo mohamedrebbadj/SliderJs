2035e8ce0a438a258e1624127e5af634e05fd414
mohamed rebbadj <mohamedaminerebbadj@gmail.com>
2022-03-08 22:12:11 +0100

    Updates

---
+++ js/index.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    // Just provide selector to the elements you want to use as range sliders
+    // Or just use the universal selector to transform all input[type='range'] in the page into SJ slider
+    const SJRanges = new SJ.init("*");
+}
